91890acb36c16c11cac36155fe1e2a18cfc70008
kenyounot123 <kenlu519@gmail.com>
2023-06-20 21:06:18 -0400

    Create html, js, and css files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+first javascript program
+Rock paper scissors will be played against the computer
---
+++ javascript.js
@@ -0,0 +1,8 @@
+//Rock paper scissors will be played against the computer 
+
+const computerChoices = ["Rock", "Paper", "Scissors"]
+
+//Randomly generate the computers choice rock,paper, or scissors
+function getComputerChoice() {
+  
+}
